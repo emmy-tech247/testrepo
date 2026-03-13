4089e318bc4a180d90c430357922b253dbe0dc24
Emmanuel <alaoemmanuel1978@gmail.com>
2026-03-13 12:15:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Family Website</title>
+</head>
+
+<body>
+
+<h1>Welcome to Our Family Website</h1>
+
+<a href="index.php">Home</a> |
+<a href="about.php">About Us</a> |
+<a href="contact.php">Contact</a> |
+<a href="register.php">Register</a> |
+<a href="login.php">Login</a>
+
+<p>This is our family website where members can register and connect.</p>
+
+</body>
+</html>
